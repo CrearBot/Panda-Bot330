d46a2fbb0db50849e007fca75948532797a5a517
CrearBot <109636577+CrearBot@users.noreply.github.com>
2022-08-01 10:17:44 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -10,8 +10,56 @@ const menu = (prefix) => {
      Creador: Leonardo
   ❒༊ֶ֪⃟⃦⃕🍁̸̷͟⸙ 𝙜𝙧𝙪𝙥𝙤 𝙙𝙚 𝙬𝙝𝙖𝙩𝙨𝘼𝙥𝙥: http://ị.li https://chat.whatsapp.com/F8r1t9CvUruE5pmqMXEUGG http://ị.li
   
-  *menu completo*
-  
+*📅 𝙵𝙴𝙲𝙷𝙰: ${week}, ${date}*
+*📈 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
+*📊 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂: ${rtotalreg}*
+
+*INFORMACIÓN*
+
+_${usedPrefix}grupos_
+_${usedPrefix}estado_
+_${usedPrefix}infobot_
+_${usedPrefix}donar_
+_${usedPrefix}grouplist_
+_${usedPrefix}owner_
+_${usedPrefix}script_
+_Bot_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
+
+*JUEGOS*
+
+_${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
+ _${usedPrefix}ppt *<papel / tijera /piedra>*_
+️ _${usedPrefix}prostituto *<nombre / @tag>*_
+_${usedPrefix}prostituta *<nombre / @tag>*_
+_${usedPrefix}gay2 *<nombre / @tag>*_
+_${usedPrefix}lesbiana *<nombre / @tag>*_
+_${usedPrefix}pajero *<nombre / @tag>*_
+_${usedPrefix}pajera *<nombre / @tag>*_
+_${usedPrefix}puto *<nombre / @tag>*_
+_${usedPrefix}puta *<nombre / @tag>*_
+_${usedPrefix}manco *<nombre / @tag>*_
+_${usedPrefix}manca *<nombre / @tag>*_
+_${usedPrefix}rata *<nombre / @tag>*_
+_${usedPrefix}love *<nombre / @tag>*_
+_${usedPrefix}doxear *<nombre / @tag>*_
+_${usedPrefix}msg *<texto>*_
+_${usedPrefix}pregunta *<texto>*_
+_${usedPrefix}slot *<apuesta>*_
+_${usedPrefix}ttt *<nombre sala>*_
+_${usedPrefix}delttt_
+_${usedPrefix}simi *<texto>*_
+_${usedPrefix}top *<texto>*_
+_${usedPrefix}topgays_
+_${usedPrefix}topotakus_
+_${usedPrefix}formarpareja_
+_${usedPrefix}verdad_
+_${usedPrefix}reto_
+_${usedPrefix}cancion_
+_${usedPrefix}pista_
+
+
+
+
 
 `
 }
