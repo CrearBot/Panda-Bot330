b7fd709d5ae577aa7b3d401bddc990d533494e4c
Leonardo <109636577+CrearBot@users.noreply.github.com>
2022-08-08 09:15:34 -0300

    Create info-sc.js

---
+++ Media/info-sc.js
@@ -0,0 +1,29 @@
+let handler = async (m, { conn, usedPrefix }) => {
+var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
+var document = doc[Math.floor(Math.random() * doc.length)]    
+let texto1 = `*https://github.com/CrearBot/Panda-Bot330*`
+let buttonMessage= {
+'document': { url: `https://github.com/CrearBot/Panda-Bot330` },
+'mimetype': `application/${document}`,
+'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': 'https://github.com/CrearBot/Panda-Bot330',
+'mediaType': 2,
+'previewType': 'pdf',
+'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+'body': wm,
+'thumbnail': imagen1,
+'sourceUrl': 'https://chat.whatsapp.com/F8r1t9CvUruE5pmqMXEUGG' }},
+'caption': texto1,
+'footer': wm,
+'buttons':[
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1}, 
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
+handler.command = ['sc','script']
+export default handler
